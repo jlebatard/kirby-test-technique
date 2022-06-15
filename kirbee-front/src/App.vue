3369902cd2c4ad23a8556cf196f136dc94468455
Julien LEBATARD <jlebatard@afdtech.com>
2022-06-15 14:52:51 +0200

    Modification back et front, parties maintenant fonctionnelles, bug avec la variable go

--- kirbee-front/src/App.vue
+++ kirbee-front/src/App.vue
@@ -2,48 +2,177 @@
   <div class="App" id="app">
     <div class="game">
       <div class="demo-scene">
-        <p>{{ status }}</p>
-        <button @click="ping">Ping</button>
+        <div class="head-game">
+          <div class="information">
+            <div v-if="status === 'full'">
+              <p>Full</p>
+            </div>
+            <div v-if="status === 'connected'">
+              <p>connected</p>
+              <button @click="useSocket('join')">Join</button>
+            </div>
+            <div v-if="status === 'joined' || status === 'playerReady'">
+              <p>Joined as {{ name }}</p>
+              <p>Waiting for a player</p>
+              <button
+                v-if="status === 'joined'"
+                @click="useSocket('playerReady')"
+              >
+                Ready
+              </button>
+            </div>
+            <div v-if="status === 'game'">
+              <p>Joined as {{ name }}</p>
+              <p>wait ...</p>
+            </div>
+            <div v-if="status === 'finish'">
+              <p>Joined as {{ name }}</p>
+              <p>{{ winner }} win!</p>
+              <button @click="useSocket('playerReady')">Ready</button>
+            </div>
+            <div v-else>
+              <p></p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <img
+            height="66"
+            :class="status !== 'go' ? 'invisible' : null"
+            src="images/action-mark.png"
+          />
+        </div>
+        <div class="playground">
+          <template v-if="status === 'finish'">
+            <div>
+              <img
+                height="66"
+                :src="
+                  winner === 'Kirby'
+                    ? 'images/knight/knight_lose.png'
+                    : 'images/knight/knight_win.png'
+                "
+              />
+            </div>
+            <div>
+              <img
+                height="66"
+                :src="
+                  winner === 'Kirby'
+                    ? 'images/kirby/kirby_win.png'
+                    : 'images/kirby/kirby_lose.png'
+                "
+              />
+            </div>
+          </template>
+          <template v-else>
+            <div>
+              <img height="66" src="images/kirby/kirby_start.png" />
+            </div>
+            <div>
+              <img height="66" src="images/knight/knight_start.png" />
+            </div>
+          </template>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import io from 'socket.io-client';
+import io from "socket.io-client";
 
 const socket = io(`${location.protocol}//${location.hostname}:3006`);
 
 export default {
-  name: 'App',
+  name: "App",
 
-  data () {
+  data() {
     return {
-      status: 'connecting...'
-    }
+      status: "connecting...",
+      position: 0,
+      name: "",
+      winner: "",
+    };
   },
 
-  mounted () {
-      socket.on('connect', () => {
-        this.status = 'connected';
-      });
-      socket.on('disconnect', () => {
-        this.status = 'disconnected';
-      });
-      socket.on('reconnect', () => {
-        this.status = 'connected';
-      });
-      socket.on('pong', () => {
-        console.log('pong received !');
-      });
+  mounted() {
+    let self = this;
+
+    // permet d'attaquer quand on appuie sur une touche
+    window.addEventListener("keyup", function () {
+      if (self.status == "game" || self.status == "go") {
+        self.useSocket("attack");
+      }
+    });
+
+    // permet d'attaquer quand on clique à la souris
+    window.addEventListener("click", function () {
+      if (self.status == "game" || self.status == "go") {
+        self.useSocket("attack");
+      }
+    });
+
+    socket.on("connect", () => {
+      this.status = "connected";
+    });
+
+    socket.on("disconnect", () => {
+      this.status = "disconnected";
+    });
+
+    socket.on("reconnect", () => {
+      this.status = "connected";
+    });
+
+    // action lorsque l'autre joueur quitte la partie
+    socket.on("playerDisconnected", () => {
+      this.position = 1;
+      this.name = "Kirby";
+    });
+
+    // action lorsque le joueur a rejoint la partie
+    socket.on("joined", (position) => {
+      this.position = position;
+      this.name = position == 1 ? "Kirby" : "Knight";
+      this.status = "joined";
+    });
+
+    // action lorsqu'un joueur est prêt
+    socket.on("playerReady", () => {
+      this.status = "playerReady";
+      this.useSocket("waiting");
+    });
+
+    // action lorsque tous les joueurs sont prêts
+    socket.on("inGame", () => {
+      this.status = "game";
+    });
+
+    // action lorsque la partie commence
+    socket.on("go", () => {
+      this.status = "go";
+    });
+
+    // action quand la partie est terminé
+    socket.on("finish", (idWinner) => {
+      this.winner = idWinner == 1 ? "Kirby" : "Knight";
+      this.status = "finish";
+    });
+
+    // action lorsque le serveur indique que le serveur est plein
+    socket.on("full", () => {
+      this.status = "full";
+    });
   },
 
   methods: {
-    ping () {
-      socket.emit('ping');
-    }
-  }
-}
+    // methode qui permet d'envoyer une requête socket
+    useSocket(value) {
+      socket.emit(value);
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -77,5 +206,22 @@ export default {
 
 .demo-scene p {
   margin-bottom: 2rem;
+}
+
+.playground {
+  display: flex;
+  justify-content: space-around;
+}
+
+.head-game {
+  height: 35%;
+}
+
+.playground img {
+  height: 66px;
+}
+
+.invisible {
+  visibility: hidden;
 }
 </style>
--- kirbee-server/server.js
+++ kirbee-server/server.js
@@ -5,19 +5,95 @@ const io = require('socket.io')(3006, {
   }
 });
 
-io.on('connection', function(socket) {
+let players = []
+let go = false // détermine si les joueurs peuvent attaquer ou non
+let finish = false // détermine si la partie est finie ou non
+
+io.on('connection', function (socket) {
   console.log('client connect - ', socket.id);
 
-  socket.on('reconnect', (attemptNumber) => {
+  // action lorsqu'un client se reconnecte
+  socket.on('reconnect', () => {
     console.log('client reconnect - ', socket.id);
   });
 
+  // action lorsqu'un client se déconnecte
   socket.on('disconnect', () => {
     console.log('client disconnect - ', socket.id);
+
+    // retire le joueur qui est parti
+    players = players.filter(player => player.id !== socket.id)
+    socket.broadcast.emit("disconnected")
   });
 
-  socket.on('ping', () => {
-    console.log('ping received');
-    socket.emit('pong');
+  // action lorsqu'un joueur rejoint la partie
+  socket.on('join', () => {
+    // partie complète
+    if (players.length >= 2) {
+      socket.emit('full')
+    }
+    // place(s) disponible(s)
+    else {
+      let player = {
+        id: socket.id,
+        ready: false
+      }
+      players.push(player)
+      socket.emit('joined', players.length);
+      console.log("Un joueur a rejoint la partie")
+    }
   });
+
+  // action quand un joueur clique sur "ready"
+  socket.on('playerReady', () => {
+    let playerReady = players.find(player => player.id == socket.id)
+    playerReady.ready = true
+    socket.emit('playerReady');
+    console.log("Le joueur " + (players.findIndex(player => player.id == socket.id)+ 1) + " a rejoint la partie")
+  });
+
+  // action lorsque les joueurs attendent
+  socket.on('waiting', async () => {
+    if (players.filter(player => player.ready === true).length >= 2) {
+      console.log("Partie commencé")
+      io.to(players[0].id).emit('inGame');
+      io.to(players[1].id).emit('inGame');
+
+      finish = false
+
+      // compte à rebours
+      await sleep(Math.floor(Math.random() * 6000) + 2000);
+      go = true
+
+      // si la partie n'est pas fini, autorise les joueurs à attaquer
+      if (!finish) {
+        console.log("les joueurs peuvent attaquer")
+        io.to(players[0].id).emit('go');
+        io.to(players[1].id).emit('go');
+      }
+    }
+  })
+
+  // action lorsqu'un joueur a attaqué
+  socket.on('attack', async () => {
+    console.log("le joueur " + (players.findIndex(player => player.id == socket.id)+ 1) + " a attaqué")
+    console.log("go", go)
+    let winner = players.findIndex(player => go ? player.id === socket.id : player.id !== socket.id) + 1
+    io.to(players[0].id).emit('finish', winner);
+    io.to(players[1].id).emit('finish', winner);
+
+    // remet les joueurs en "non-prêt"
+    players.forEach(player => player.ready = false)
+    console.log("partie terminée")
+    go = false
+    finish = true
+  })
 });
+
+
+// fonction qui permet d'attendre
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
